801dca3e583673f0ff6108cc346d4d81cfafa6b9
Julio Murta <souzamurtaj@gmail.com>
2020-02-12 20:14:22 -0300

    Armstrong numbers solved

--- exercises/armstrong-numbers/src/armstrong_numbers.clj
+++ exercises/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,5 +1,11 @@
 (ns armstrong-numbers)
 
-(defn armstrong? [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn parse-bigint [s]
+  (BigInteger. (re-find #"[0-9]*" s)))
+
+(defn armstrong? [num] 
+  (let [sequence (map #(parse-bigint (str %)) (vec (str num))) 
+        exp (count sequence)]  
+    (let [calculated-vec (map #(Math/pow % exp) sequence)]
+      (let [total (reduce + (map #(parse-bigint (str %)) calculated-vec))]
+        (= num total)))))
