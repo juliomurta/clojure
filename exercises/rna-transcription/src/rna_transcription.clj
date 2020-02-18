2f29fc153e8384c7dbb07c4bfefe112ab57e09c4
Julio Murta <souzamurtaj@gmail.com>
2020-02-18 14:44:44 -0300

    RNA transcription solved

--- exercises/rna-transcription/src/rna_transcription.clj
+++ exercises/rna-transcription/src/rna_transcription.clj
@@ -1,5 +1,15 @@
 (ns rna-transcription)
 
-(defn to-rna [dna] ;; <- arglist goes here
-  ;; your code goes here
+(defn get-gene [gene]
+  (def dna-to-rna { :G "C" :C "G" :T "A" :A "U"})
+  (let [result (get dna-to-rna (keyword (str gene)))]
+    (if (= result nil)
+      (throw (AssertionError. "invalid"))
+      result
+    )
+  )
+)
+
+(defn to-rna [dna]   
+  (apply str (map get-gene dna))
 )
