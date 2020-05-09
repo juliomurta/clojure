2a14fc969f206ca039a92c096448f1aefe0c1dea
Julio Murta <souzamurtaj@gmail.com>
2020-05-08 22:45:56 -0300

    anagram solved

--- exercises/anagram/src/anagram.clj
+++ exercises/anagram/src/anagram.clj
@@ -1,8 +1,18 @@
 (ns anagram)
 
+(defn- to-lower
+  [word]
+  (clojure.string/lower-case word))
+
+(defn- same-letters-number
+  [word prospect]
+  (= (frequencies (to-lower word)) (frequencies (to-lower prospect))))
+
 (defn- is-anagram? 
   [word prospect]
-  (if (and (not (= word prospect)) (every? true? (map #(.contains word (str %)) prospect)))
+  (if (and (not (= (to-lower word) (to-lower prospect))) 
+                (every? true? (map #(.contains (to-lower word) (to-lower (str %))) prospect))
+                (same-letters-number word prospect))
     prospect))
 
 (defn anagrams-for [word prospect-list] 
--- exercises/say/src/say.clj
+++ exercises/say/src/say.clj
@@ -22,9 +22,7 @@
     (= num 16) "sixteen"
     (= num 17) "seventeen"
     (= num 18) "eighteen"
-    (= num 19) "nineteen"
-  )
-)
+    (= num 19) "nineteen"))
 
 (defn- between-99-and-21
   [num]
@@ -36,68 +34,54 @@
     (= 6 (quot num 10)) "sixty"
     (= 7 (quot num 10)) "seventy"
     (= 8 (quot num 10)) "eighty"
-    (= 9 (quot num 10)) "ninety"
-  )
-)
+    (= 9 (quot num 10)) "ninety"))
 
 (defn- between-999-and-100
   [num]
   (if (and (>= num 100) (< num 999))
-    (str (numbers-below-20 (quot num 100)) " hundred")
-  )
-)
+    (str (numbers-below-20 (quot num 100)) " hundred")))
 
 (defn- insert-hifen? 
   [num]
   (if (and (> num 20 )
            (< num 100)
-           (not (= num 0))) "-" "")
-)
+           (not (= num 0))) "-"))
 
 (defn- insert-space? 
   [num limit]
-  (if (> num limit) " " "")
-)
+  (if (> num limit) " "))
 
 (defn- extract-mod
   [num divided-by]
-  (mod num (* (quot num divided-by) divided-by))
-)
+  (mod num (* (quot num divided-by) divided-by)))
 
 (defn- extract-billion
   [num]
-  (if (>= num 1000000000000) (extract-mod num 1000000000000) num)
-)
+  (if (>= num 1000000000000) (extract-mod num 1000000000000) num))
 
 (defn- extract-million
   [num]
-  (if (>= num 1000000000) (extract-mod num 1000000000) num)
-)
+  (if (>= num 1000000000) (extract-mod num 1000000000) num))
 
 (defn- extract-thousand 
   [num]
-  (if (>= num 1000000) (extract-mod num 1000000) num)
-)
+  (if (>= num 1000000) (extract-mod num 1000000) num))
 
 (defn- extract-hundred 
   [num]
-  (if (>= num 1000) (extract-mod num 1000) num)
-)
+  (if (>= num 1000) (extract-mod num 1000) num))
 
 (defn- extract-ten 
   [num]
-  (if (>= num 100) (extract-mod num 100) num)
-)
+  (if (>= num 100) (extract-mod num 100) num))
 
 (defn- extract-unity
   [num]
-  (if (>= num 10) (mod num 10) num)
-)
+  (if (>= num 10) (mod num 10) num))
 
 (defn- extract-unity-if-more-than-20
   [num]
-  (if (> num 20) (extract-unity num) num)
-)
+  (if (> num 20) (extract-unity num) num))
 
 (defn- build-string-until-999
   [hundred ten until-20]  
@@ -106,31 +90,30 @@
        (between-99-and-21 ten)        
        (insert-hifen? ten) 
        (if (not (and (= until-20 0) (> (+ hundred (+ ten until-20)) 20)))
-          (numbers-below-20 until-20))
-  )
-)
+          (numbers-below-20 until-20))))
 
 (defn- between-999999-and-1000
   [num]
-  (def bt-hundred (extract-hundred (quot num 1000)))
-  (def bt-ten (extract-ten bt-hundred))
-  (def bt-until-20 (extract-unity-if-more-than-20 bt-ten))  
-  (str (build-string-until-999 bt-hundred bt-ten bt-until-20) " thousand")  
-)
+  (def th-hundred (extract-hundred (quot num 1000)))
+  (def th-ten (extract-ten th-hundred))
+  (def th-until-20 (extract-unity-if-more-than-20 th-ten))  
+  (str (build-string-until-999 th-hundred th-ten th-until-20) " thousand"))
 
 (defn- between-999999999-and-1000000
   [num]
+  (def mi-hundred (extract-hundred (quot num 1000000)))
+  (def mi-ten (extract-ten mi-hundred))
+  (def mi-until-20 (extract-unity-if-more-than-20 mi-ten))  
   (if (and (>= num 1000000) (< num 999999999))    
-    (str (numbers-below-20 (quot num 1000000)) " million")
-  )
-)
+    (str (build-string-until-999 mi-hundred mi-ten mi-until-20) " million")))
 
 (defn- between-999999999999-and-1000000000
   [num]
+  (def bi-hundred (extract-hundred (quot num 1000000000)))
+  (def bi-ten (extract-ten bi-hundred))
+  (def bi-until-20 (extract-unity-if-more-than-20 bi-ten))  
   (if (and (>= num 1000000000) (< num 999999999999))
-    (str (numbers-below-20 (quot num 1000000000)) " billion")
-  )
-)
+    (str (build-string-until-999 bi-hundred bi-ten bi-until-20) " billion")))
 
 (defn- build-string 
   [num]
@@ -142,20 +125,15 @@
   (def until-20 (extract-unity-if-more-than-20 num))
   (str (if (>= num 1000000000) (between-999999999999-and-1000000000 billion))
        (if (> num 1000000000) " ")
-       (if (>= num 1000000) (between-999999999-and-1000000 million))
-       (if (and (> num 1000000) (<= num 999999999)) " ")       
+       (if (>= num 1000000) (between-999999999-and-1000000 million)) 
+       (if (> million 1000000) " ")       
        (if (and (>= num 1000) (> thousand 0)) (between-999999-and-1000 thousand))
        (if (and (> num 1000) (> thousand 0)) " ")       
        (if (or (not (= 0 (mod num 1000))) (= num 0))
-        (build-string-until-999 hundred ten until-20)
-       )
-  )
-)
+        (build-string-until-999 hundred ten until-20))))
 
 (defn number 
   [num] 
   (if (or (< num 0) (> num 999999999999))
     (throw (IllegalArgumentException. "Out of range"))    
-    (build-string num)
-  )
-)
+    (build-string num)))
