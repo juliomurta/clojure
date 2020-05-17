a9bb179cb17a4db77070021ec3c3dc5e0f188ee8
Julio Murta <souzamurtaj@gmail.com>
2020-05-17 17:17:09 -0300

    starting isbn verifier

--- exercises/isbn-verifier/src/isbn_verifier.clj
+++ exercises/isbn-verifier/src/isbn_verifier.clj
@@ -1,5 +1,21 @@
 (ns isbn-verifier)
 
-(defn isbn? [isbn] ;; <- arglist goes here
-  ;; your code goes here
-)
+;(def invalid-char #{"a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "y" "z"})
+
+(defn- sum 
+  [isbn]
+  (reduce + (for [x [10 9 8 7 6 5 4 3 2 1]]
+    (* (read-string (str (get isbn (- 10 x)))) x))))
+
+;(defn- is-valid 
+;  [value]
+;  (contains? value invalid-char))
+
+(defn isbn? [isbn] 
+  (let [value (clojure.string/replace isbn "-" "")]
+    (if (not (= (count value) 10))
+      (false)
+      (= (mod (sum value) 11) 0))))
+ ;     (if (not (is-valid isbn))
+ ;     (false)
+  
