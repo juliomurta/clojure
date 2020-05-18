72c452cd2dfba46676b96f77bfbb2b3acc8f4d2d
Julio Murta <souzamurtaj@gmail.com>
2020-05-17 23:25:27 -0300

    improving isbn

--- exercises/isbn-verifier/src/isbn_verifier.clj
+++ exercises/isbn-verifier/src/isbn_verifier.clj
@@ -1,21 +1,30 @@
 (ns isbn-verifier)
 
-;(def invalid-char #{"a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "y" "z"})
+(def invalid-char #{"a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "y" "z"})
+
+(defn- if-x-change-to-10
+  [x]
+  (if (= (clojure.string/lower-case x) "x") "10" x))
+
+(defn- get-item
+  [isbn position]
+  (read-string (if-x-change-to-10 (str (get isbn (- 10 position))))))
 
 (defn- sum 
   [isbn]
   (reduce + (for [x [10 9 8 7 6 5 4 3 2 1]]
-    (* (read-string (str (get isbn (- 10 x)))) x))))
+    (* (get-item isbn x) x))))
 
-;(defn- is-valid 
-;  [value]
-;  (contains? value invalid-char))
+(defn- has-invalid-char
+  [isbn]
+  (if (not (= (some true? (map #(clojure.string/includes? isbn (clojure.string/lower-case %)) invalid-char)) nil)) 
+    true 
+    false))
 
-(defn isbn? [isbn] 
+(defn isbn? 
+  [isbn] 
   (let [value (clojure.string/replace isbn "-" "")]
-    (if (not (= (count value) 10))
-      (false)
+    (if (or (not (= (count value) 10)) (has-invalid-char isbn))
+      false
       (= (mod (sum value) 11) 0))))
- ;     (if (not (is-valid isbn))
- ;     (false)
   
